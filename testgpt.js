d8f814843f1534562aa10301fad5e8533dcbaa90
rustjavi <rustjavi@proton.me>
2024-05-26 02:51:32 -0400

    Update testgpt.js

--- testgpt.js
+++ testgpt.js
@@ -1,6 +1,19 @@
 const os = require('os');
 const dotenv = require('dotenv');
+const path = require('path');
 const { setCookies } = require('./g4f/cookies');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+    
+});
 
 dotenv.load();
 
